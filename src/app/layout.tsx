51a16f80c7ec6ce87e3c614bf3c8dfbcbd84c70c
Chris Boydstun <chrisboydstun@gmail.com>
2024-10-15 08:45:43 -0500

    add ga tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,15 @@ import { Analytics } from "@vercel/analytics/react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./styles/global.css";
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 export const metadata = {
   title: "Part 107 Drone License Quiz App",
@@ -15,8 +24,35 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (GA_MEASUREMENT_ID) {
+      const url =
+        pathname + (searchParams.toString() ? "?" + searchParams.toString() : "");
+      window.gtag("config", GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    }
+  }, [GA_MEASUREMENT_ID, pathname, searchParams]);
+
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen bg-gradient-to-br from-blue-100 via-white to-purple-100">
         <ApolloWrapper>
           <Navbar />
