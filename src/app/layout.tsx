d496d3aed23f090a0eda0a2cecd970e1ca8cd86c
Chris Boydstun <chrisboydstun@gmail.com>
2024-10-15 08:55:27 -0500

    refactored analytics stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
-"use client";
+// src/app/layout.tsx
 
-import { ApolloWrapper } from "../components/ApolloWrapper";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "../components/GoogleAnalytics";
+import { ApolloWrapper } from "../components/ApolloWrapper";
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import "./styles/global.css";
-import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import Script from "next/script";
 
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void;
-  }
-}
+import "./styles/global.css";
 
 export const metadata = {
   title: "Part 107 Drone License Quiz App",
@@ -27,41 +22,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (GA_MEASUREMENT_ID) {
-      const url =
-        pathname + (searchParams.toString() ? "?" + searchParams.toString() : "");
-      window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
-      });
-    }
-  }, [GA_MEASUREMENT_ID, pathname, searchParams]);
 
   return (
     <html lang="en">
       <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `}
-        </Script>
+        {GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="gtag-init" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              `}
+            </Script>
+          </>
+        )}
       </head>
       <body className="flex flex-col min-h-screen bg-gradient-to-br from-blue-100 via-white to-purple-100">
         <ApolloWrapper>
           <Navbar />
+          <GoogleAnalytics />
           <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-12 max-w-7xl">
             {children}
-            <Analytics />
           </main>
+          <Analytics />
           <Footer />
         </ApolloWrapper>
       </body>
---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,30 @@
+// src/components/Analytics.tsx
+
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+declare global {
+  interface Window {
+    gtag: (command: string, id: string, config: Record<string, any>) => void;
+  }
+}
+
+export default function Analytics() {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (GA_MEASUREMENT_ID) {
+      const url =
+        pathname + (searchParams.toString() ? '?' + searchParams.toString() : '');
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    }
+  }, [GA_MEASUREMENT_ID, pathname, searchParams]);
+
+  return null;
+}
